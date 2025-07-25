187ebadaa37d3d61cc9c772d5196826730c54671
Rafael Trias <rafatrias.27@gmail.com>
2025-07-25 04:05:55 -0400

    update navbar carrito

--- views/components/nav.js
+++ views/components/nav.js
@@ -26,27 +26,28 @@ const createNavHome = () => {
           <svg xmlns="http://www.w3.org/2000/svg" id="shop-icon" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class=" transition ease-in-out w-10 h-10 cursor-pointer hover:bg-pink-900 rounded-lg">
           <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 10.5V6a3.75 3.75 0 1 0-7.5 0v4.5m11.356-1.993 1.263 12c.07.665-.45 1.243-1.119 1.243H4.25a1.125 1.125 0 0 1-1.12-1.243l1.264-12A1.125 1.125 0 0 1 5.513 7.5h12.974c.576 0 1.059.435 1.119 1.007ZM8.625 10.5a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Zm7.5 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Z" />
           </svg>
-        <section class="absolute top-20 r-10 w-100% h-25 bg-slate-300">
-  <table>
-    <thead>
-      <tr>
-        <th class="table-title">Imagen</th>
-        <th class="table-title">Nombre</th>
-        <th class="table-title">Precio</th>
-        <th class="table-title">Cantidad</th>
-      </tr>
-    </thead>
-    <tbody id="table-body"></tbody>
-    <tr>
-      <td>Imagen</td>
-      <td>Programcion</td>
-      <td>15$</td>
-      <td>4</td>
-    </tr>
-  </table>
-  <button id="table-remove">Vaciar Carrito</button>
-</section> 
 
+          <section class="absolute top-20 r-10 w-100% h-25 bg-slate-300 flex flex-col justify-between">
+            <table class="table-fixed text-center border-collapse">
+              <thead>
+                <tr>
+                  <th class="table-title">Imagen</th>
+                  <th class="table-title">Nombre</th>
+                  <th class="table-title">Precio</th>
+                  <th class="table-title">Cantidad</th>
+                </tr>
+              </thead>
+              <tbody id="table-body"></tbody>
+                <tr>
+                  <td class="h-25 px-1 py-0 border-b bg-solid">Imagen</td>
+                  <td>Programcion</td>
+                  <td>15$</td>
+                  <td>4</td>
+                </tr>
+            </table>
+        <button id="table-remove">Vaciar Carrito</button>
+          </section>  
+        
 
         <a href="/login" class="transition ease-in-out text-white font-bold bg-pink-800 hover:bg-pink-900 py-2 px-4 rounded-lg">Login</a>
           <a href="/signup" class="transition ease-in-out text-white font-bold bg-pink-800 hover:bg-pink-900 py-2 px-4 rounded-lg">Sign Up</a>
