b17f5f1ac3a485a563865fedc0b4781ba89d2b2c
Rafael Trias <rafatrias.27@gmail.com>
2025-08-08 01:51:37 -0400

    update localhost config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const PAGE_URL = process.env.NODE_ENV === 'production'
-    ? ''
+    ? 'https://localhost:3000'
     : 'http://localhost:3000';
 
 const MONGO_URI = process.env.NODE_ENV === 'production'
